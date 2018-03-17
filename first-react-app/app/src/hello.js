357a24e16a1370233af0054aa01ce8853ed98633
Birlax Shukla <birlax@gmail.com>
2018-03-17 15:11:26 +0530

    Adding Inline-CSS/External CSS support to project Reviewer:Self

--- first-react-app/app/src/hello.js
+++ first-react-app/app/src/hello.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
- 
+import './style.css';
+
 class Hello extends React.Component {
   render() {
-    return <h2>Hdfdfdfddfgdfgdfgdgfello</h2>
+    return <h2 className="hello">Hdfdfdfddfgdfgdfgdgfello</h2>
   }
 }
  
--- first-react-app/webpack.config.js
+++ first-react-app/webpack.config.js
@@ -11,8 +11,8 @@ module.exports = {
       },
       { test: /\.css$/,
         use: [
-          { loader: "style-loader" },
-          { loader: "css-loader" }
+          'style-loader',
+          'css-loader'
         ]
       }	
     ]
